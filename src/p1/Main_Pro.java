7854885c700847a35881e948a6527430f2eb64f1
AkankshaMankar <89790286+Aquamankar@users.noreply.github.com>
2023-11-16 16:34:37 +0530

    Update Main_Pro.java
    added one more file

--- src/p1/Main_Pro.java
+++ src/p1/Main_Pro.java
@@ -3,6 +3,7 @@ package p1;
 public class Main_Pro {
 	public static void main(String[] args) {
 		System.out.println("this is a java program run by jenkins");
+		System.out.println("added one more on jenkins");
 	}
 	
 
